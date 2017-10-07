ad5d24a5bd27839d3c611d204ad7509a3449564e
Andrew Zhang <andy.zhang@berekeley.edu>
2017-10-07 10:50:52 -0700

    debug

--- public/default.js
+++ public/default.js
@@ -1,19 +1,5 @@
 
 (function () {
-    var app = require('express');
-    var http = require('http').Server(app);
-    var io = require('socket.io')(http);
-
-    app.get('/', function(req, res){
-      res.sendFile(__dirname + '/index.html');
-    });
-
-    io.on('connection', function(socket){
-      socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-      });
-    });
-    
     WinJS.UI.processAll().then(function () {
       
       var socket, serverGame;
